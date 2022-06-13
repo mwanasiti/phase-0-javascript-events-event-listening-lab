b6500807864553dfbfc7b281ec6d69190ca014d2
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-06-13 21:44:56 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
 }
+const input = document.getElementById('input');
+
+function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    return "clicked"
+  });
+}
